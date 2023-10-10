fa4d382a5a9e70d3564b8b4eb36c04dac7292a82
Tetiana Vrba <tetiana.vrba@gmail.com>
2023-10-10 14:42:48 +0200

    JS

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-console.log('Funguju!')
+console.log('Funguju!');
 
 const textPozpatku = (text) => {
-	return text.split('').reverse().join('')
-}
+  return text.split('').reverse().join('');
+};
 
 // textPozpatku('Ahoj světe') // 'etěvs johA'
 // textPozpatku('Kajak') // 'kajaK'
+const polozky = document.querySelectorAll('li');
+polozky.forEach((polozka) => {
+  const text = polozka.textContent.toLocaleLowerCase().replaceAll(' ', '');
+  if (text === textPozpatku(text)) {
+    polozka.classList.add('palindrom');
+  }
+});
